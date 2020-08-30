08f6ed2639659c6f0682f7f016e43b47c30b33a8
tarunkolla <tarunkolla@gmail.com>
2020-08-30 16:28:38 -0500

    remove dev redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,8 +8,8 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //redux chrome extention req
+    applyMiddleware(thunk)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //redux chrome extention req
   )
 );
 
